7b5c99ba4987c1ba43b55e5bdae8d0e8d0d4163a
Yigit Boyar <yboyar@gmail.com>
2021-04-26 19:30:42 -0700

    pff

--- app/src/main/kotlin/com/birbit/actions/github/sample/main.kt
+++ app/src/main/kotlin/com/birbit/actions/github/sample/main.kt
@@ -8,7 +8,7 @@ import io.ktor.server.netty.*
 fun main(args: Array<String>) {
     println("hello ${args.joinToString(",")}")
     embeddedServer(Netty, port = 8080,
-        host = "0.0.0.0",
+        host = "127.0.0.1",
         configure = {
 
         }) {
