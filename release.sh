94aa49c4d5efdc0f746e0374feeb1426712aaa9d
Yigit Boyar <yboyar@gmail.com>
2021-05-15 20:32:50 -0700

    never learn bash :)

--- release.sh
+++ release.sh
@@ -4,10 +4,14 @@ echo $GITHUB_REF
 VERSION="${GITHUB_REF##*/}"
 echo "version: $VERSION"
 
+if [[ -z "${VERSION}" ]]; then
+   echo "no version found, cancel OP"
+   exit 1
+fi
 # set git
 if [[ -z "${CI}" ]]; then
   echo "Not in ci, use current user"
-elif
+else
   git config --local user.email "github-deplay-action@users.noreply.github.com"
   git config --local user.name "github-deplay-action"
 fi
