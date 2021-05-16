e061b194c0de5ba3cde1eb5f6010ca032c967d1b
Yigit Boyar <yboyar@gmail.com>
2021-05-15 20:19:44 -0700

    lets see if this works

--- release.sh
+++ release.sh
@@ -3,3 +3,10 @@ set -e
 echo $GITHUB_REF
 VERSION="${GITHUB_REF##*/}"
 echo "version: $VERSION"
+git checkout --orphan $VERSION
+git add dist
+git add action.yml
+git add LICENSE
+git commit -m "Release $VERSION from $GITHUB_SHA"
+git push origin $VERSION
+
