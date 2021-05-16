c17ff46e69ed189ae2653a9bdc98822dd8ecdff6
Yigit Boyar <yboyar@gmail.com>
2021-05-15 20:29:23 -0700

    set username email

--- release.sh
+++ release.sh
@@ -3,6 +3,15 @@ set -e
 echo $GITHUB_REF
 VERSION="${GITHUB_REF##*/}"
 echo "version: $VERSION"
+
+# set git
+if [[ -z "${CI}" ]]; then
+  echo "Not in ci, use current user"
+elif
+  git config --local user.email "github-deplay-action@users.noreply.github.com"
+  git config --local user.name "github-deplay-action"
+fi
+
 git checkout --orphan $VERSION
 git add dist
 git add action.yml
