42bcdaf206772cbed40d3bafd803fa0455d772f6
Yigit Boyar <yboyar@google.com>
2021-06-10 14:56:19 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 echo "PR NUMBER: $PR_NUMBER"
+
+echo "event path: $GITHUB_EVENT_PATH"
+echo "event path contents:"
+cat $GITHUB_EVENT_PATH
