b861015555c16c0f0de2182a690a5173084e6b33
Yigit Boyar <yboyar@google.com>
2021-06-11 12:37:04 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-PUSH_BASE_SHA=${{ github.event.before }}
-PR_BASE_SHA=${{ github.event.pull_request.base.sha }}
-
-echo "$PUSH_BASE_SHA / $PR_BASE_SHA"
-PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
-echo "PR NUMBER: $PR_NUMBER"
-
 echo "event path: $GITHUB_EVENT_PATH"
 echo "event path contents:"
 COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
-echo "compare: $COMPARE"
+echo "compare url: $COMPARE"
 # api.github.com/repos
 COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
-echo $COMPARE_API
-COMPARE=$(curl -v -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
-echo "compare $COMPARE"
+echo "compare API url: $COMPARE_API"
+COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
+echo "compare response: $COMPARE_RESPONSE"
 # statuses we are interested in: added, modified, renamed
-FILES=$(echo $COMPARE | jq '.files | .[] | select(.status != "removed") | .filename')
-echo $FILES
-
-for file in "${FILES[@]}"
-do
-  echo "file: ${file}"
-done
+CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq '.files | .[] | select(.status != "removed") | .filename') | sed 's/"//g'
+echo $CHANGED_FILES
