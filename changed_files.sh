26f260a3d078df2e3666a3a42a3cee40629b27b0
Yigit Boyar <yboyar@google.com>
2021-06-11 12:50:36 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "event path: $GITHUB_EVENT_PATH"
-echo "event path contents:"
+#echo "event path: $GITHUB_EVENT_PATH"
+#echo "event path contents:"
 COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
-echo "compare url: $COMPARE"
+#echo "compare url: $COMPARE"
 # api.github.com/repos
 COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
-echo "compare API url: $COMPARE_API"
+#echo "compare API url: $COMPARE_API"
 COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
-echo "compare response: $COMPARE_RESPONSE"
+#echo "compare response: $COMPARE_RESPONSE"
 # statuses we are interested in: added, modified, renamed
 CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq '.files | .[] | select(.status != "removed") | .filename')
 for file in "${CHANGED_FILES[@]}"
@@ -15,4 +15,4 @@ do
   # remove quotes
   echo $file | sed 's/"//g' 
 done
-echo "::set-output name=files::$CHANGED_FILES"
+#echo "::set-output name=files::$CHANGED_FILES"
