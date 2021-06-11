e49624142cbe91061cfbb6b7dfd410590739ffe4
Yigit Boyar <yboyar@google.com>
2021-06-11 14:07:11 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -9,7 +9,8 @@ COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| se
 COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
 echo "compare response: $COMPARE_RESPONSE"
 # statuses we are interested in: added, modified, renamed
-CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq '.files | .[] | select(.status != "removed") | .filename')
+CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq -r '.files | .[] | select(.status != "removed") | .filename')
+echo "changed files: $CHANGED_FILES"
 LINE_DELIMITED_FILES=$(IFS='\n'; echo "${CHANGED_FILES[*]}" )
 echo "listing files"
 for file in "${CHANGED_FILES[@]}"
@@ -19,6 +20,6 @@ do
   echo $file | sed 's/"//g' 
 done
 echo "done listing files"
-echo $LINE_DELIMITED_FILES
+echo "line delimited files: $LINE_DELIMITED_FILES"
 
 echo "::set-output name=files::$LINE_DELIMITED_FILES"
