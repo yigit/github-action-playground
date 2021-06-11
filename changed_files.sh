bf6ac40abebfa133e9d33c3a00b9ba01fa0b2df0
Yigit Boyar <yboyar@google.com>
2021-06-11 14:29:09 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #echo "event path: $GITHUB_EVENT_PATH"
 #echo "event path contents:"
-COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
+#COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
 #echo "compare url: $COMPARE"
 # api.github.com/repos
-COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
+#COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
 #echo "compare API url: $COMPARE_API"
-COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
-# COMPARE_RESPONSE=$(cat compare.json)
-echo "compare response: $COMPARE_RESPONSE"
+#COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
+COMPARE_RESPONSE=$(cat compare.json)
+#echo "compare response: $COMPARE_RESPONSE"
 # statuses we are interested in: added, modified, renamed
-CHANGED_FILES=`echo $COMPARE_RESPONSE | jq -r '.files | .[] | select(.status != "removed") | .filename'`
+CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq -r '.files | .[] | select(.status != "removed") | .filename')
 echo "changed files: $CHANGED_FILES"
 # echo "changed files expanded: $CHANGED_FILES"
 # LINE_DELIMITED_FILES=$(IFS=' '; echo "${CHANGED_FILES[*]}" )
