9c5cbcdf44507894fce5d73cd98e95bde7531248
Yigit Boyar <yboyar@google.com>
2021-06-11 16:57:18 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -2,13 +2,31 @@
 echo "event path: $GITHUB_EVENT_PATH"
 cat $GITHUB_EVENT_PATH
 COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
-echo "compare url: $COMPARE"
-# api.github.com/repos
-COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
-echo "compare API url: $COMPARE_API"
-COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
-echo "compare response: $COMPARE_RESPONSE"
-# statuses we are interested in: added, modified, renamed. basically, anything but removed
-CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq -r '.files | .[] | select(.status != "removed") | .filename' | tr '\r\n' ' ')
+PR=$(jq -r '.pull_request._links.self.href' $GITHUB_EVENT_PATH)
+echo "-----"
+echo "compare: |$COMPARE|"
+echo "PR: |$PR|"
+echo "------"
+if [[ $COMPARE != null ]]
+then
+    echo "compare url is set, using it"
+    # api.github.com/repos
+    COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
+    echo "compare API url: $COMPARE_API"
+    COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
+    echo "compare response: $COMPARE_RESPONSE"
+    # statuses we are interested in: added, modified, renamed. basically, anything but removed
+    CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq -r '.files | .[] | select(.status != "removed") | .filename' | tr '\r\n' ' ')
+elif [[ $PR != null ]]
+then
+    echo "this is a PR, using that"
+    PR_FILES_API="$PR/files"
+    PR_FILES_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $PR_FILES_API)
+    CHANGED_FILES=$(echo $PR_FILES_RESPONSE | jq -r '.[] | select(.status != "removed") | .filename' | tr '\r\n' ' ')
+else
+    echo "CANNOT FIND CHANGED FILES"
+    exit 1
+fi
+
 echo "changed files: $CHANGED_FILES"
 echo "::set-output name=files::$CHANGED_FILES"
