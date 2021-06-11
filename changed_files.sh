f0efaaabd66d505c3eb69daa44e6d29ad3a718ed
Yigit Boyar <yboyar@google.com>
2021-06-11 12:25:19 -0700

    does jq work?

--- changed_files.sh
+++ changed_files.sh
@@ -8,4 +8,18 @@ echo "PR NUMBER: $PR_NUMBER"
 
 echo "event path: $GITHUB_EVENT_PATH"
 echo "event path contents:"
-cat $GITHUB_EVENT_PATH
+COMPARE=$(jq '.compare' $GITHUB_EVENT_PATH)
+echo "compare: $COMPARE"
+# api.github.com/repos
+COMPARE_API=$(echo $COMPARE | sed 's/github.com\//api.github.com\/repos\//g'| sed 's/"//g')
+echo $COMPARE_API
+COMPARE=$(curl -v -H "Accept: application/vnd.github.v3+json" $COMPARE_API)
+echo "compare $COMPARE"
+# statuses we are interested in: added, modified, renamed
+FILES=$(echo $COMPARE | jq '.files | .[] | select(.status != "removed") | .filename')
+echo $FILES
+
+for file in "${FILES[@]}"
+do
+  echo "file: ${file}"
+done
