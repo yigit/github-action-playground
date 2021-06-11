31a7bb14e3c330097ea1f2225cf72e43494d6deb
Yigit Boyar <yboyar@google.com>
2021-06-11 12:47:00 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -10,9 +10,9 @@ COMPARE_RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" $COMPARE_API
 echo "compare response: $COMPARE_RESPONSE"
 # statuses we are interested in: added, modified, renamed
 CHANGED_FILES=$(echo $COMPARE_RESPONSE | jq '.files | .[] | select(.status != "removed") | .filename')
-echo $CHANGED_FILES
 for file in "${CHANGED_FILES[@]}"
 do
   # remove quotes
   echo $file | sed 's/"//g' 
 done
+echo "::set-output name=files::$CHANGED_FILES"
