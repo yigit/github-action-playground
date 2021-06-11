477b65fb70a587ed9470cc537662188600257fff
Yigit Boyar <yboyar@google.com>
2021-06-11 08:53:25 -0700

    more

--- changed_files.sh
+++ changed_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+PUSH_BASE_SHA=${{ github.event.before }}
+PR_BASE_SHA=${{ github.event.pull_request.base.sha }}
+
+echo "$PUSH_BASE_SHA / $PR_BASE_SHA"
 PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 echo "PR NUMBER: $PR_NUMBER"
 
